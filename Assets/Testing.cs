f11e2a62e1b7b94d820154701e474f92c1fd0830
joshuavanbreda <joshuavanbreda@gmail.com>
2022-01-04 00:34:04 +0200

    1

---
+++ Assets/Testing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Transform[] points;
+    [SerializeField] private LineController line;
+    private void Start()
+    {
+        line.SetUpLine(points);
+    }
+}
